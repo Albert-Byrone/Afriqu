47897b5d5d900b10a23c85fb546fd2dd5a11e7a9
albertbyrone <albertbyrone1677@gmail.coim>
2020-02-27 21:13:02 +0300

    create email credentials

--- login/emails.py
+++ login/emails.py
@@ -6,8 +6,8 @@ def send_credentials(password,name,receiver):
     subject = 'Taste Afrique PAssword Management.'
     sender = 'albertbyrone1677@gmail.com'
 
-    text_content = render_to_string('email/passwordemail.txt', {"password":password, "username":name})
-    html_content = render_to_string('email/passwordemail.html', {"password":password, "username":name})
+    text_content = render_to_string('email/passwordemail.txt', {"password":password, "name":name})
+    html_content = render_to_string('email/passwordemail.html', {"password":password, "name":name})
 
     msg = EmailMultiAlternatives((subject,text_content,sender))
     msg.attach_alternative(html_content, 'text/html')
